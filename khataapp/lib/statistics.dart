846cd7a187ece47173420cdbdfc3e3160f93a54d
muhammadhamx <muhammadhamzasajid6@gmail.com>
2023-12-18 21:43:13 +0500

    updated

--- khataapp/lib/statistics.dart
+++ khataapp/lib/statistics.dart
@@ -8,39 +8,39 @@ class StatisticsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          // Leading Icon
-          leading: IconButton(
+        elevation: 0,
+        // Leading Icon
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.black,
+            size: 30,
+          ),
+        ),
+        // Tilte
+        title: const Text(
+          'Home',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        // More Icons
+        actions: [
+          IconButton(
             onPressed: () {},
             icon: const Icon(
-              Icons.menu,
-              color: Colors.black,
-              size: 30,
-            ),
-          ),
-          // Tilte
-          title: const Text(
-            'Home',
-            style: TextStyle(
-              fontSize: 20,
+              Icons.search,
               color: Colors.black,
             ),
           ),
-          centerTitle: true,
-          // More Icons
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.search,
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-    )
-  };
+        ],
+      ),
+    );
+  }
 }
