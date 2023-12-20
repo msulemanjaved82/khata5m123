109d61f9f0b9ff66bdcaabc6fe1070b11ede535b
muhammadhamx <muhammadhamzasajid6@gmail.com>
2023-12-21 00:41:19 +0500

    updated

--- khataapp/lib/homepage.dart
+++ khataapp/lib/homepage.dart
@@ -1,156 +1,26 @@
 // this is home page
-import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:khataapp/homepagei.dart';
+import 'package:khataapp/statistics.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentPage = 0;
+  List<Widget> pages = const [HomePageItems(), StatisticsScreen()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          // Leading Icon
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.menu,
-              color: Colors.black,
-              size: 30,
-            ),
-          ),
-          // Tilte
-          title: const Text(
-            'Home',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
-            ),
-          ),
-          centerTitle: true,
-          // More Icons
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.search,
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-        body: const SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Welcome,',
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                Text(
-                  'Muhammad Hamza',
-                  style: TextStyle(
-                    fontSize: 17,
-                    color: Colors.black54,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Placeholder(
-                  fallbackHeight: 199,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Easy Operations',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 0),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      EasyOperations(
-                        icon: Icons.change_circle_outlined,
-                        label: 'Change',
-                      ),
-                      EasyOperations(
-                        icon: Icons.arrow_upward,
-                        label: 'Transfer',
-                      ),
-                      EasyOperations(
-                        icon: Icons.arrow_downward,
-                        label: 'Income',
-                      ),
-                      EasyOperations(
-                        icon: Icons.paste,
-                        label: 'Receipt',
-                      ),
-                      EasyOperations(
-                        icon: Icons.read_more,
-                        label: 'More',
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 22),
-                Text(
-                  'Previous Transactions',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 12),
-                Column(
-                  children: [
-                    PreviousTransactions(
-                        transIcon: Icons.shopping_basket,
-                        itemname: 'Market',
-                        date: '21 Aug 2023',
-                        amount: '-3,500',
-                        colorM: Colors.red),
-                    PreviousTransactions(
-                        transIcon: Icons.home_repair_service,
-                        itemname: 'Home Appliances',
-                        date: '19 Aug 2023',
-                        amount: '-9,500',
-                        colorM: Colors.red),
-                    PreviousTransactions(
-                        transIcon: Icons.currency_bitcoin,
-                        itemname: 'Trading income',
-                        date: '14 Aug 2023',
-                        amount: '+15,000',
-                        colorM: Colors.blue),
-                    PreviousTransactions(
-                        transIcon: Icons.car_repair,
-                        itemname: 'Car Mantainance',
-                        date: '8 Aug 2023',
-                        amount: '-5,500',
-                        colorM: Colors.red),
-                    PreviousTransactions(
-                        transIcon: Icons.attach_money,
-                        itemname: 'Office Income',
-                        date: '2 Aug 2023',
-                        amount: '+60,000',
-                        colorM: Colors.blue),
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
+        body:
+            currentPage == 0 ? const HomePageItems() : const StatisticsScreen(),
+
         // Bottom Navigation
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
@@ -160,10 +30,13 @@ class HomePage extends StatelessWidget {
               const Color.fromARGB(255, 0, 0, 0).withOpacity(.6),
           selectedFontSize: 14,
           unselectedFontSize: 14,
+          currentIndex: currentPage,
           onTap: (value) {
-            // Respond to item press.
+            setState(() {
+              currentPage = value;
+            });
           },
-          items: const <BottomNavigationBarItem>[
+          items: const [
             BottomNavigationBarItem(
               label: 'Home',
               icon: Icon(Icons.home),
@@ -182,96 +55,5 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ));
-  }
-}
-
-// Easy Operations constructor
-class EasyOperations extends StatelessWidget {
-  final IconData icon;
-  final String label;
-  const EasyOperations({
-    super.key,
-    required this.icon,
-    required this.label,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          width: 80,
-          height: 80,
-          child: Card(
-            elevation: 4,
-            child: Container(
-              padding: const EdgeInsets.all(0),
-              child: IconButton(
-                icon: Icon(
-                  icon,
-                  size: 30,
-                  color: const Color.fromARGB(255, 99, 99, 99),
-                ),
-                onPressed: () {},
-                alignment: Alignment.center,
-              ),
-            ),
-          ),
-        ),
-        Text(label),
-      ],
-    );
-  }
-}
-
-class PreviousTransactions extends StatelessWidget {
-  final IconData transIcon;
-  final String itemname;
-  final String date;
-  final String amount;
-  final Color colorM;
-  const PreviousTransactions({
-    super.key,
-    required this.transIcon,
-    required this.itemname,
-    required this.date,
-    required this.amount,
-    required this.colorM,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 70,
-      child: Card(
-        elevation: 16,
-        child: ListTile(
-          leading: Icon(transIcon, size: 30.0),
-          title: Column(
-            children: [
-              Text(
-                itemname,
-                style: const TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 3),
-              Text(
-                date,
-                style: const TextStyle(fontSize: 15),
-              ),
-            ],
-          ),
-          trailing: Text(
-            'Rs $amount',
-            style: TextStyle(
-              fontSize: 20,
-              color: colorM,
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
--- khataapp/lib/homepagei.dart
+++ khataapp/lib/homepagei.dart
@@ -1,14 +1,18 @@
-//this is Statistics screen
-//i will create this screen- Musab.
+// This is home Screen
+// I create this -- Hamza
 import 'package:flutter/material.dart';
 
-class StatisticsScreen extends StatelessWidget {
-  const StatisticsScreen({super.key});
+class HomePageItems extends StatefulWidget {
+  const HomePageItems({super.key});
 
+  @override
+  State<HomePageItems> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HomePageItems> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
@@ -23,7 +27,7 @@ class StatisticsScreen extends StatelessWidget {
         ),
         // Tilte
         title: const Text(
-          'All Transactions',
+          'Home',
           style: TextStyle(
             fontSize: 20,
             color: Colors.black,
@@ -49,104 +53,96 @@ class StatisticsScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                'Rs 5,00,000',
+                'Welcome,',
                 style: TextStyle(
                   fontSize: 22,
                   fontWeight: FontWeight.w700,
                 ),
               ),
               Text(
-                'My Total Earning',
+                'Muhammad Hamza',
                 style: TextStyle(
                   fontSize: 17,
                   color: Colors.black54,
                 ),
               ),
-              SizedBox(
-                height: 29,
+              SizedBox(height: 10),
+              Placeholder(
+                fallbackHeight: 199,
               ),
-              Row(children: [
-                Text(
-                  'All My Expenses',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w700,
-                  ),
+              SizedBox(height: 10),
+              Text(
+                'Easy Operations',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-              ]),
+              ),
+              SizedBox(height: 0),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    EasyOperations(
+                      icon: Icons.change_circle_outlined,
+                      label: 'Change',
+                    ),
+                    EasyOperations(
+                      icon: Icons.arrow_upward,
+                      label: 'Transfer',
+                    ),
+                    EasyOperations(
+                      icon: Icons.arrow_downward,
+                      label: 'Income',
+                    ),
+                    EasyOperations(
+                      icon: Icons.paste,
+                      label: 'Receipt',
+                    ),
+                    EasyOperations(
+                      icon: Icons.read_more,
+                      label: 'More',
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 22),
+              Text(
+                'Previous Transactions',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 12),
               Column(
                 children: [
-                  Expenses(
+                  PreviousTransactions(
                       transIcon: Icons.shopping_basket,
                       itemname: 'Market',
                       date: '21 Aug 2023',
                       amount: '-3,500',
                       colorM: Colors.red),
-                  Expenses(
+                  PreviousTransactions(
                       transIcon: Icons.home_repair_service,
                       itemname: 'Home Appliances',
                       date: '19 Aug 2023',
                       amount: '-9,500',
                       colorM: Colors.red),
-                  Expenses(
-                      transIcon: Icons.currency_bitcoin,
-                      itemname: 'Trading loss',
-                      date: '14 Aug 2023',
-                      amount: '-15,000',
-                      colorM: Colors.blue),
-                  Expenses(
-                      transIcon: Icons.car_repair,
-                      itemname: 'Car Mantainance',
-                      date: '8 Aug 2023',
-                      amount: '-5,500',
-                      colorM: Colors.red),
-                  Expenses(
-                      transIcon: Icons.electric_meter,
-                      itemname: 'Electicity Bills',
-                      date: '2 Aug 2023',
-                      amount: '-60,000',
-                      colorM: Colors.blue),
-                ],
-              ),
-              SizedBox(
-                height: 29,
-              ),
-              Row(children: [
-                Text(
-                  'All My Earnings',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ]),
-              Column(
-                children: [
-                  Income(
-                      transIcon: Icons.share,
-                      itemname: 'Market Shares',
-                      date: '21 Aug 2023',
-                      amount: '+3,500',
-                      colorM: Colors.red),
-                  Income(
-                      transIcon: Icons.sell,
-                      itemname: 'Land Selling',
-                      date: '19 Aug 2023',
-                      amount: '+2,55,000',
-                      colorM: Colors.red),
-                  Income(
+                  PreviousTransactions(
                       transIcon: Icons.currency_bitcoin,
                       itemname: 'Trading income',
                       date: '14 Aug 2023',
                       amount: '+15,000',
                       colorM: Colors.blue),
-                  Income(
-                      transIcon: Icons.currency_bitcoin,
-                      itemname: 'Trading',
+                  PreviousTransactions(
+                      transIcon: Icons.car_repair,
+                      itemname: 'Car Mantainance',
                       date: '8 Aug 2023',
-                      amount: '+90,500',
+                      amount: '-5,500',
                       colorM: Colors.red),
-                  Income(
+                  PreviousTransactions(
                       transIcon: Icons.attach_money,
                       itemname: 'Office Income',
                       date: '2 Aug 2023',
@@ -162,65 +158,52 @@ class StatisticsScreen extends StatelessWidget {
   }
 }
 
-class Expenses extends StatelessWidget {
-  final IconData transIcon;
-  final String itemname;
-  final String date;
-  final String amount;
-  final Color colorM;
-  const Expenses({
+// Easy Operations constructor
+class EasyOperations extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  const EasyOperations({
     super.key,
-    required this.transIcon,
-    required this.itemname,
-    required this.date,
-    required this.amount,
-    required this.colorM,
+    required this.icon,
+    required this.label,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 70,
-      child: Card(
-        elevation: 16,
-        child: ListTile(
-          leading: Icon(transIcon, size: 30.0),
-          title: Column(
-            children: [
-              Text(
-                itemname,
-                style: const TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
+    return Column(
+      children: [
+        SizedBox(
+          width: 80,
+          height: 80,
+          child: Card(
+            elevation: 4,
+            child: Container(
+              padding: const EdgeInsets.all(0),
+              child: IconButton(
+                icon: Icon(
+                  icon,
+                  size: 30,
+                  color: const Color.fromARGB(255, 99, 99, 99),
                 ),
+                onPressed: () {},
+                alignment: Alignment.center,
               ),
-              const SizedBox(height: 3),
-              Text(
-                date,
-                style: const TextStyle(fontSize: 15),
-              ),
-            ],
-          ),
-          trailing: Text(
-            'Rs $amount',
-            style: const TextStyle(
-              fontSize: 20,
-              color: Colors.red,
             ),
           ),
         ),
-      ),
+        Text(label),
+      ],
     );
   }
 }
 
-class Income extends StatelessWidget {
+class PreviousTransactions extends StatelessWidget {
   final IconData transIcon;
   final String itemname;
   final String date;
   final String amount;
   final Color colorM;
-  const Income({
+  const PreviousTransactions({
     super.key,
     required this.transIcon,
     required this.itemname,
@@ -255,9 +238,9 @@ class Income extends StatelessWidget {
           ),
           trailing: Text(
             'Rs $amount',
-            style: const TextStyle(
+            style: TextStyle(
               fontSize: 20,
-              color: Colors.lightBlueAccent,
+              color: colorM,
             ),
           ),
         ),
